6f3d337605421935d652b381595cfc6c2e326842
Brandon <bmaxwell921@gmail.com>
2013-12-04 17:10:13 -0600

    Implemented set up for UserProcesses

--- Project2/src/edu/iastate/cs352/project2/main/simVm.java
+++ Project2/src/edu/iastate/cs352/project2/main/simVm.java
@@ -1,5 +1,7 @@
 package edu.iastate.cs352.project2.main;
 
+import edu.iastate.cs352.project2.logic.UserProcess;
+
 /**
  * Main class used to fire up the simulation
  * @author Brandon
@@ -7,7 +9,50 @@ package edu.iastate.cs352.project2.main;
  */
 public class simVm {
 
+	private static int size, numPages, numFrames, numUserProc;
+	private static final boolean DEBUGGING = true;
+	
 	public static void main(String args[]) {
-		// TODO
+		if (args.length != 4) {
+			System.out.println("ERROR: Not enough program arguments. Should be 4 but was: " + args.length);
+			System.exit(0);
+		}
+		
+		size = numPages = numFrames = numUserProc = 0;
+		
+		try {
+			size = Integer.parseInt(args[0]);
+			numPages = Integer.parseInt(args[1]);
+			numFrames = Integer.parseInt(args[2]);
+			numUserProc = Integer.parseInt(args[3]);
+		} catch (NumberFormatException nfe) {
+			System.out.println("ERROR: Program arguments are not all integers!");
+			System.exit(0);
+		}
+		
+		if (DEBUGGING) {
+			overwriteValues();
+		}
+		
+		// TODO set up MemoryManagementUnit
+		
+		// FIRE 'EM UP!
+		for (int i = 0; i < numUserProc; ++i) {
+			final UserProcess proc = new UserProcess(i, size, numPages);
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					proc.run();
+				}
+			}).start();
+		}
+		
+	}
+
+	private static void overwriteValues() {
+		size = 8;
+		numPages = 2;
+		numFrames = 2;
+		numUserProc	= 1;
 	}
 }
