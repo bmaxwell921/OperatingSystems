c6ffd7721ba7af60db61bbbb32052f0f46680dd9
Brandon <bmaxwell921@gmail.com>
2013-12-04 23:45:29 -0600

    Fixed outputs to look pretty :3

--- Project2/src/edu/iastate/cs352/project2/logging/VirtualMemLogger.java
+++ Project2/src/edu/iastate/cs352/project2/logging/VirtualMemLogger.java
@@ -12,28 +12,28 @@ import edu.iastate.cs352.project2.info.ThreadInfo;
 public class VirtualMemLogger {
 
 	// Message parts
-	private static final String PROCESS_MSG = "[Process %d] ";
-	private static final String ADDRESS_MSG = "address %d ";
-	private static final String PAGE_NUM_MSG = "(page number = %d, ";
-	private static final String PAGE_OFFSET_MSG = "page offset = %d) ";
-	private static final String FRAME_NUM_MSG = "(frame number = f)";
+	private static final String PROCESS_MSG = "Process %d";
+	private static final String ADDRESS_MSG = "address %d";
+	private static final String PAGE_NUM_MSG = "page number = %d";
+	private static final String PAGE_OFFSET_MSG = "page offset = %d";
+	private static final String FRAME_NUM_MSG = "frame number = %d";
 	
 	// Actual outputs
-	private static final String SUCCESS_ACCESS_MSG = PROCESS_MSG + "accesses " + ADDRESS_MSG + PAGE_NUM_MSG
-			+ PAGE_OFFSET_MSG + "in main memory " + FRAME_NUM_MSG;
+	private static final String SUCCESS_ACCESS_MSG = "[" + PROCESS_MSG + "] " + "accesses " + ADDRESS_MSG + " " + "(" + PAGE_NUM_MSG + ", "
+			+ PAGE_OFFSET_MSG + ") "+ "in main memory " + "(" + FRAME_NUM_MSG + ").";
 	
-	private static final String PAGE_FAULT_MSG = PROCESS_MSG + "accesses " + ADDRESS_MSG + PAGE_NUM_MSG
-			+ PAGE_OFFSET_MSG + "not in main memory.";
+	private static final String PAGE_FAULT_MSG = "[" + PROCESS_MSG + "] " + "accesses " + ADDRESS_MSG + " " + "(" + PAGE_NUM_MSG + ", "
+			+ PAGE_OFFSET_MSG + ") " + "not in main memory.";
 	
-	private static final String FOUND_FRAME_MSG = PROCESS_MSG + "finds a free frame in main memory " 
-			+ FRAME_NUM_MSG;
-	private static final String REPLACE_FRAME_MSG = PROCESS_MSG + "replaces a frame " + FRAME_NUM_MSG
+	private static final String FOUND_FRAME_MSG = "[" + PROCESS_MSG + "] " + "finds a free frame in main memory " 
+			+ "(" + FRAME_NUM_MSG + ").";
+	private static final String REPLACE_FRAME_MSG = "[" + PROCESS_MSG + "] " + "replaces a frame " + "(" + FRAME_NUM_MSG  + ")"
 			+ " from the main memory.";
-	private static final String ISSUE_SWAP_MSG = PROCESS_MSG + "issues an I/O operation to swap in demanded page "
-			+ PAGE_NUM_MSG;
-	private static final String COMPLETE_SWAP_MSG = PROCESS_MSG + "demanded page " + PAGE_NUM_MSG
-			+ "has been swapped in main memory " + FRAME_NUM_MSG + ".";
-	private static final String PROCESS_ENDS_MSG = PROCESS_MSG + "ends.";
+	private static final String ISSUE_SWAP_MSG = "[" + PROCESS_MSG + "] " + "issues an I/O operation to swap in demanded page "
+			+ "(" + PAGE_NUM_MSG + ").";
+	private static final String COMPLETE_SWAP_MSG = "[" + PROCESS_MSG + "] " + "demanded page " + "(" + PAGE_NUM_MSG
+			+ ") has been swapped in main memory " + "(" + FRAME_NUM_MSG + ").";
+	private static final String PROCESS_ENDS_MSG = "[" + PROCESS_MSG + "] " + "ends.";
 	
 	/**
 	 * Used to log when an address is found already in main memory
