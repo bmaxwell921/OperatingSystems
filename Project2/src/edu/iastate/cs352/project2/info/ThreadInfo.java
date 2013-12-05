073368cf7cf3233284aeda3a450b75a83b051b97
Brandon <bmaxwell921@gmail.com>
2013-12-04 21:12:18 -0600

    Generated equals and hashcode implementation

--- Project2/src/edu/iastate/cs352/project2/info/ThreadInfo.java
+++ Project2/src/edu/iastate/cs352/project2/info/ThreadInfo.java
@@ -16,4 +16,26 @@ public class ThreadInfo {
 	public int getProcessId() {
 		return this.processId;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + processId;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ThreadInfo other = (ThreadInfo) obj;
+		if (processId != other.processId)
+			return false;
+		return true;
+	}
 }
